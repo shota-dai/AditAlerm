f1fdab0ca6ae88caf6e888955476f1fc64d935c8
Shota Dai <shota.dai@isana.net>
2020-05-16 22:43:36 +0900

    Add a view to clock in

---
+++ AditAlerm/ClockInView.swift
@@ -0,0 +1,62 @@
+//
+//  ClockInView.swift
+//  AditAlerm
+//
+//  Created by Shota Dai on 2020/05/16.
+//  Copyright © 2020 Shota Dai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClockInView: View {
+    
+    let popover: NSPopover?
+    
+    // define clock out time as Now+9h
+    @State private var selectedDate = Date(timeIntervalSinceNow: 60*60*9)
+    
+    var body: some View {
+        VStack {
+            Text("おはようございます！")
+                .font(.system(size: 20))
+                .bold()
+            Spacer()
+                .frame(height: 20)
+            Text("退勤予定時刻を設定後、\n「打刻する」もしくは「閉じる」を押してください。")
+            Spacer()
+                .frame(height: 20)
+            DatePicker(selection: $selectedDate, in: Date()..., displayedComponents: .hourAndMinute) {
+                Text("退勤予定時刻")
+            }
+            Spacer()
+                .frame(height: 40)
+            HStack {
+                Button(action: {
+                    // TODO: save selected clock out time
+                    
+                    self.popover?.close()
+                }){
+                    Text("閉じる")
+                        .font(.system(size: 15))
+                }
+                Spacer()
+                    .frame(width: 40)
+                Button(action: {
+                    // TODO: save selected clock out time
+                    // TODO: adit automatically
+                }){
+                    Text("打刻する")
+                        .font(.system(size: 15))
+                }
+            }
+        }
+        .padding(20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct ClockInView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClockInView(popover: nil)
+    }
+}
